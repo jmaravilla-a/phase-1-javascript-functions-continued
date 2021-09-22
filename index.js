5e92c7634af89a1332581a5e07ae3c60f1224b72
Javier Maravilla <javier.maravilla@outlook.com>
2021-09-22 11:02:11 -0400

    commit tree e303d0df743ce3c6ca7beb6fce4752154f272406

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+
+function mondayWork(activity = "go to the office") {
+    return (`This Monday, I will ${activity}.`);
+}
+
+/*
+function wrapAdjective(string = "*") {
+    const part1 = "You are";
+    return function () {
+      const part2 = "special";
+      return function () {
+        console.log(`${part1} ${string} ${part2} ${string}!`);
+      };
+    };
+  }
+*/
+
+function wrapAdjective(a = "*") {
+    return function (b = "special") {
+        return `You are ${a}${b}${a}!`
+    };
+}
